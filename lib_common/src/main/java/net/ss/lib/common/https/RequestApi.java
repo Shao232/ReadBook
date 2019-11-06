d38e0a89242ade3734d3b2223a2ac3cb6dcc7e19
1147083532@qq.com <1147083532@qq.com>
2019-11-06 09:49:12 +0800

    修改忽略文件

--- lib_common/src/main/java/net/ss/lib/common/https/RequestApi.java
+++ lib_common/src/main/java/net/ss/lib/common/https/RequestApi.java
@@ -7,8 +7,19 @@ package net.ss.lib.common.https;
  */
 public interface RequestApi {
 
+    /**
+     * 网址
+     */
     String BASE_URL = "https://dynic.anchengcn.com/";
 
+    /**
+     * 登录
+     */
     String LOGIN = BASE_URL+"Login.aspx?";
+
+    /**
+     * 书架
+     */
+    String BOOK_SHELF = BASE_URL +"Bookshelf.aspx";
 
 }
